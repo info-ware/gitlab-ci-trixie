47937d41ad01cc3be940f694b4a8988a8dedcf25
Robin Syllwasschy <syllwasschy@infoware.de>
2025-11-17 12:44:43 +0100

    Updated Dockerfile to include libpq, changed permissions back to root

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-docker build -t infoware/gitlab-ci-bookworm .
+
+#export IWREGISTRY=88.99.167.70:5000
+#sudo docker buildx build   -t $IWREGISTRY/infoware/gitlab-ci-bookworm:latest   --cache-to=type=registry,ref=infoware/gitlab-ci-bookworm:buildcache,mode=max   --cache-from=type=registry,ref=infoware/gitlab-ci-bookworm:buildcache --load .
+sudo docker buildx build   -t infoware/gitlab-ci-bookworm:latest   --cache-to=type=registry,ref=infoware/gitlab-ci-bookworm:buildcache,mode=max   --cache-from=type=registry,ref=infoware/gitlab-ci-bookworm:buildcache --push .
